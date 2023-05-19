dca4afbd676ab81add59c4e9fd32820cec1c5bb8
Jackson Hoffart <jackson.hoffart@gmail.com>
2023-05-19 17:36:15 +0200

    tdm_conditions_met gains unit test

---
+++ tests/testthat/test-tdm_conditions_met.R
@@ -0,0 +1,22 @@
+test_that("passes TRUE, if at least one group of data contains all TDM years", {
+  t0 <- 2021
+  delta_t1 <- 5
+  delta_t2 <- 9
+  scenarios <- "IPR FPS 2021"
+
+  input_data <- fake_tdm_data(
+    ald_sector = c("power", "power", "power", "steel"),
+    year = c(t0, t0 + delta_t1, t0 + delta_t2, t0)
+  )
+
+  out <- tdm_conditions_met(
+    input_data,
+    t0,
+    delta_t1,
+    delta_t2,
+    scenarios,
+    project_code = "GENERAL"
+    )
+
+  expect_equal(out, TRUE)
+})
